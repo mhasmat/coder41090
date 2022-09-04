723914685da66a4dea85e5bf0061c67b3216ad8a
Martin Hasmat <mhasmat@gmail.com>
2022-09-03 22:26:33 -0300

    simulador

---
+++ simulador-interactivo/app.js
@@ -0,0 +1,20 @@
+
+// let usuarioGuardado = 'martin';
+// let nombreIngresado = '';
+
+// let contador = 0;
+// while (nombreIngresado !== usuarioGuardado && contador <3){
+//     nombreIngresado = prompt("Ingrese su nombre de usuario");
+//     contador++;
+// }
+
+// if (nombreIngresado === usuarioGuardado) {
+//     document.write(`Hola ${usuarioGuardado.toUpperCase()}, bienvenido!`);
+// } else {
+//     document.write('El usuario es incorrecto. Permiso denegado.');
+// }
+
+
+
+
+
