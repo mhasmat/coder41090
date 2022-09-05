2e615b3996021d5e7f4338985d862cc7fd1b8f4f
Martin Hasmat <mhasmat@gmail.com>
2022-09-05 19:27:48 -0300

    avance 05/9

--- simulador-interactivo/app.js
+++ simulador-interactivo/app.js
@@ -1,20 +1,25 @@
+let usuarioGuardado = 'martin'; //el usuario válido
+let nombreIngresado = ''; //acá se guarda lo que el usuario tipea en el prompt
+let contador = 0; //contador de intentos de login
 
-// let usuarioGuardado = 'martin';
-// let nombreIngresado = '';
+//funcion que cuenta los intentos de login
+function intentosLogin(){
+    while (nombreIngresado !== usuarioGuardado && contador <3){
+        nombreIngresado = prompt("Ingrese su nombre de usuario");
+        contador++;
+    }
+}
 
-// let contador = 0;
-// while (nombreIngresado !== usuarioGuardado && contador <3){
-//     nombreIngresado = prompt("Ingrese su nombre de usuario");
-//     contador++;
-// }
+//funcion que valida el nombre de usuario
+//para ver si coincide con el q está guardado
+function validarUsuario() {
+    if (nombreIngresado === usuarioGuardado) {
+        document.write(`Hola ${usuarioGuardado.toUpperCase()}, bienvenido!`);
+    } else {
+        document.write('El usuario es incorrecto. Permiso denegado.');
+    }    
+}
 
-// if (nombreIngresado === usuarioGuardado) {
-//     document.write(`Hola ${usuarioGuardado.toUpperCase()}, bienvenido!`);
-// } else {
-//     document.write('El usuario es incorrecto. Permiso denegado.');
-// }
-
-
-
-
-
+//invocando las funciones
+intentosLogin();
+validarUsuario();
