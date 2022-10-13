3e3584c01d8ba6f9eab37e6fe0d3647e25568528
Marto <mhasmat@gmail.com>
2022-10-12 22:58:38 -0300

    agregando fetch

--- simulador-interactivo/app.js
+++ simulador-interactivo/app.js
@@ -39,132 +39,58 @@ const botonSesion = document.getElementById('sesion');
 //evento a disparar con el boton
 botonSesion.addEventListener('click', iniciarSesion);
 
-// //seccion tarjetas
-// const apiData = 'productos.json';
-// //capturo el contenedor de las tarjetas
-// const contenedor = document.getElementById('contenedor-productos');
-
-// //llamada al endpoint donde está la data
-// fetch(`${apiData}`)
-//   .then((response) => response.json())
-//   .then((productos) => {
-//     //recorro el array de objetos con el forEach
-//     productos.forEach((producto) => {
-//       //creo un elemento de tipo div
-//       const div = document.createElement('div');
-//       //le agrego la clase producto al div
-//       div.classList.add('producto');
-//       //inyecto la data en el html
-//       div.innerHTML = `
-//               <img src=${producto.img} alt="${producto.nombre}" id="img-tarjetas">
-//               <h4>${producto.nombre}</h4>
-//               <p>${producto.descripcion}</p>
-//               <p>${producto.precio}</p>
-//               <button class="btn btn-warning btn-comprar">Comprar</button>
-//               <button class="btn btn-success btn-carrito">Carrito</button>
-//               `;
-//       //le agrego el hijo(div) al padre(contenedor)
-//       contenedor.appendChild(div);
-
-//       //funcion ampliar
-//       let visor = document.getElementById('visor-img');
-//       let imgTarjetas = document.getElementById('img-tarjetas');
-//       productos.forEach(() => {
-//         imgTarjetas.addEventListener('click', function ampliar() {
-//           imgTarjetas.style.transform = 'scale(1.9)';
-//         });
-//       });
-//     });
-//   });
-
-const stock = [
-  {
-    id: 1,
-    nombre: 'Cupcakes',
-    precio: 500,
-    minimo: 6,
-    inventario: 20,
-    descripcion: 'saraza',
-    img: 'img/cupcakes.jpg',
-  },
-  {
-    id: 2,
-    nombre: 'Alfajorcitos',
-    precio: 100,
-    minimo: 12,
-    inventario: 40,
-    descripcion: 'saraza',
-    img: 'img/alfajorcitos.jpg',
-  },
-  {
-    id: 3,
-    nombre: 'Petit Fours',
-    precio: 800,
-    minimo: 6,
-    inventario: 20,
-    descripcion: 'saraza',
-    img: 'img/petitFours.png',
-  },
-  {
-    id: 4,
-    nombre: 'Cookies',
-    precio: 900,
-    minimo: 12,
-    inventario: 40,
-    descripcion: 'saraza',
-    img: 'img/cookies.jpg',
-  },
-];
+//seccion tarjetas
+//capturo el contenedor de las tarjetas
+const contenedor = document.getElementById('contenedor-productos');
 
 let lista = document.querySelector('ul#listaCarrito');
 let temp = document.querySelector('template');
 let card = temp.content.querySelector('.card');
 
-stock.forEach((elemento) => {
-  let cardClonada = card.cloneNode(true);
-  cardClonada.querySelector('.card-img-top').src = elemento.img;
-  cardClonada.querySelector('h4').innerText = elemento.nombre;
-  cardClonada.querySelector('p').innerText = elemento.descripcion;
-  cardClonada.querySelector('.card-footer').innerText = `$ ${elemento.precio}`;
+fetch('productos.json')
+  .then((response) => response.json())
+  .then((productos) => {
+    productos.forEach((elemento) => {
+      let cardClonada = card.cloneNode(true);
+      cardClonada.querySelector('.card-img-top').src = elemento.img;
+      cardClonada.querySelector('h4').innerText = elemento.nombre;
+      cardClonada.querySelector('p').innerText = elemento.descripcion;
+      cardClonada.querySelector(
+        '.card-footer'
+      ).innerText = `$ ${elemento.precio}`;
 
-  lista.appendChild(cardClonada);
+      lista.appendChild(cardClonada);
 
-  cardClonada.querySelector('button').addEventListener('click', () => {
-    //agregando sweetalert2
-    Swal.fire({
-      position: 'top-start',
-      icon: 'success',
-      title: 'Se agregó al carrito!',
-      showConfirmButton: false,
-      timer: 2000
-    })
+      cardClonada.querySelector('button').addEventListener('click', () => {
+        agregarAlCarrito(elemento.id, 1);
+
+        //agregando sweetalert2
+        Swal.fire({
+          position: 'top-start',
+          icon: 'success',
+          title: 'Se agregó al carrito!',
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      });
+    });
   });
-});
 
 //guardar en localStorage
 const guardarEnLocal = (clave, valor) => {
   localStorage.setItem(clave, valor);
 };
 
-guardarEnLocal('listaProductos', JSON.stringify(stock));
-
-const almacenado = JSON.parse(localStorage.getItem('listaProductos'));
-console.log('Almacenado:', almacenado);
-
 //carrito
 let carrito = [];
 
-const agregarAlCarrito = () => {
-  let productoId = Number(prompt('ID del producto:'));
-  let cantidad = Number(prompt('Cantidad:'));
-  let producto = stock.find((item) => item.id === productoId);
+const agregarAlCarrito = (productoId, cantidad) => {
+  let producto = producto.find((item) => item.id === productoId);
   producto.cantidad = cantidad;
   producto.total = producto.precio * cantidad;
   carrito.push(producto);
+  mostrarCarrito(carrito);
 };
-
-agregarAlCarrito();
-agregarAlCarrito();
 
 const calcularTotal = (carrito) => {
   let total = 0;
@@ -182,16 +108,14 @@ const mostrarCarrito = (products) => {
   contenedorCarrito.innerHTML = `<h3>Carrito</h3>`;
 
   products.forEach((product) => {
-    const card = document.createElement('card');
-    card.innerHTML += `<div>                      
+    const carritoItem = document.createElement('carritoItem');
+    carritoItem.innerHTML += `                      
                       <h5>${product.nombre}</h5>
                       <p>Precio: $ ${product.precio}</p>
                       <p>Cantidad: ${product.cantidad}</p>
                       <hr>
-                      </div>`;
+                      `;
 
-    contenedorCarrito.appendChild(card);
+    contenedorCarrito.appendChild(carritoItem);
   });
 };
-
-mostrarCarrito(carrito);
